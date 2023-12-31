b7d48fc52d0668a83de83c4438212ff940958d04
Petar Vasilev <petar@pshow.org>
2023-12-31 05:52:01 +0000

    search implemented

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Link } from "@chakra-ui/next-js";
+import React, { useState } from "react";
+
 import { useRouter } from "next/navigation";
 import {
     Box,
@@ -14,12 +15,19 @@ import {
     Image,
     Text,
     Button,
+    InputRightElement,
+    IconButton,
 } from "@chakra-ui/react";
-import { AttachmentIcon, Search2Icon } from "@chakra-ui/icons";
+import {
+    AttachmentIcon,
+    CloseIcon,
+    Search2Icon,
+    SmallCloseIcon,
+} from "@chakra-ui/icons";
+import { search, sortKind } from "fast-fuzzy";
 
 import { NotebookMetadata } from "@/definitions/Notebook";
 import NotebookCard from "./notebookCard";
-import { notebookToUrl } from "@/lib/nav";
 
 const EmptyGallery = () => {
     const router = useRouter();
@@ -51,51 +59,100 @@ const EmptyGallery = () => {
     );
 };
 
-const SearchBar = () => (
-    <Box w="full" bg="white" rounded="md">
-        <InputGroup>
-            <InputLeftElement pointerEvents="none">
-                <Search2Icon boxSize="1.2em" color="gray.300" />
-            </InputLeftElement>
-            <Input
-                bg="white"
-                placeholder="Search in the gallery"
-                size="lg"
-                variant="filled"
-            />
-        </InputGroup>
-    </Box>
-);
+const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
+    const [value, setValue] = useState("");
+    const update = (val: string) => {
+        onSearch && onSearch(val);
+        setValue(val);
+    };
+    return (
+        <Box w="full" bg="white" rounded="md">
+            <InputGroup size="md">
+                <InputLeftElement pointerEvents="none">
+                    <Search2Icon color="gray.300" />
+                </InputLeftElement>
+                <Input
+                    pr="6rem"
+                    bg="white"
+                    onChange={(event) => update(event.target.value.trim())}
+                    placeholder="Type to search in the gallery"
+                    value={value}
+                    variant="filled"
+                />
+                {value.length > 0 && (
+                    <InputRightElement>
+                        <IconButton
+                            aria-label="Clear Search"
+                            icon={<SmallCloseIcon />}
+                            onClick={() => update("")}
+                            size="sm"
+                            variant="solid"
+                            // colorScheme="blackAlpha"
+                        />
+                    </InputRightElement>
+                )}
+            </InputGroup>
+        </Box>
+    );
+};
+
+const filterNotebooks = (
+    notebooks: NotebookMetadata[],
+    query: string,
+): NotebookMetadata[] => {
+    const keySelector = ({ title, author, tags }: NotebookMetadata) => [
+        title,
+        author || "",
+        ...tags,
+    ];
+    const results = search(query, notebooks, {
+        keySelector,
+        returnMatchData: true,
+        ignoreCase: true,
+        sortBy: sortKind.bestMatch,
+    });
+    return results.map((match) => match.item);
+};
+
+const NotebooksDisplay = ({ notebooks }: { notebooks: NotebookMetadata[] }) => {
+    const [filtered, setFiltered] = useState(notebooks);
+
+    const onSearch = (query: string) => {
+        if (query.length === 0) {
+            return setFiltered(notebooks);
+        }
+        setFiltered(filterNotebooks(notebooks, query));
+    };
+
+    return (
+        <>
+            <SearchBar onSearch={onSearch} />
+            {/* TODO: maybe use wrap with wrap element */}
+            <SimpleGrid
+                templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+                w="full"
+                spacing={4}
+            >
+                {filtered.map((book, i) => (
+                    // TODO: add highlight for matched strings
+                    <NotebookCard notebook={book} key={`notebook-${i}`} />
+                ))}
+            </SimpleGrid>
+        </>
+    );
+};
 
 export default function Gallery({
     notebooks,
 }: {
     notebooks: NotebookMetadata[];
 }) {
-    const router = useRouter();
-
-    const openNotebook = (nmd: NotebookMetadata) => {
-        router.push(notebookToUrl(nmd));
-    };
     const hasNotebooks = notebooks.length > 0;
-
     return (
         <VStack align="center" direction="column" p={5} spacing={4}>
             <Heading>EDS Books Gallery</Heading>
-
-            {hasNotebooks && <SearchBar />}
-
-            <Box w="full" maxW="lg" bg="white" shadow="lg" rounded="md"></Box>
             {hasNotebooks ? (
-                <SimpleGrid
-                    templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-                    w="full"
-                    spacing={4}
-                >
-                    {notebooks.map((book, i) => (
-                        <NotebookCard notebook={book} key={`notebook-${i}`} />
-                    ))}
-                </SimpleGrid>
+                <NotebooksDisplay notebooks={notebooks} />
             ) : (
                 <EmptyGallery />
             )}
